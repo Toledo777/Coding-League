419e91f87139cf989be3fe3ae23ed89ddccbc915
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-06 09:48:05 -0500

    Further simplification

--- build/recursiveDirUtils.mjs
+++ build/recursiveDirUtils.mjs
@@ -1,20 +1,23 @@
 import * as fs from 'fs/promises';
 
-
 async function rawRecursiveWatch(path, onChange) {
+	let children = await fs.readdir(path);
 
-	await fs.readdir(path)
-		.then(async children => {
-			children.forEach(child => recursiveWatch(`${path + '/' + child}`, onChange));
-			for await (let change of fs.watch(path)) { // eslint-disable-line
-				onChange();
-			}
-		})
-		.catch(() => { });
+	children.forEach(async (child) => {
+		const childPath = `${path}/${child}`;
+		if ((await fs.lstat(childPath)).isDirectory()) {
+			recursiveWatch(childPath, onChange);
+		}
+	});
+
+	// eslint-disable-next-line
+	for await (let change of fs.watch(path)) {
+		onChange();
+	}
 }
 
 // Watches a given directory for changes recursively.
-export async function recursiveWatch(path, onChange, delay = 500) {
+export async function recursiveWatch(path, onChange, delay = 100) {
 	let timeout = 0;
 	rawRecursiveWatch(path, () => {
 		clearTimeout(timeout);
