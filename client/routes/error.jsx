9bd7fad9df06db46c3b92a76b3fa85c94054534b
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-06 15:57:10 -0500

    Fallback error handlin

--- client/routes/error.jsx
+++ client/routes/error.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
 export default function Error() {
-	const {status, error: {message} } = useRouteError();
-	return <div className='error-page'>
-		<h1>{status}</h1>
-		<div>{message}</div>
-	</div>;
+	try {
+		const {status, error: {message} } = useRouteError();
+		return <div className='error-page'>
+			<h1>{status}</h1>
+			<div>{message}</div>
+		</div>;
+	} catch {
+		return <h1>Error</h1>;
+	}
+
 }
