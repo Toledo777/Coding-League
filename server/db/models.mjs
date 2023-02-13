6a7a34ce375f61fd3983d1831c2ec72530cdcd5f
Matthew Toledo <matthew.toledo20@gmail.com>
2023-02-13 12:50:30 -0500

    update models

--- server/db/models.mjs
+++ server/db/models.mjs
@@ -1,8 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const testSchema = mongoose.Schema({
 	user: String,
 	comment: String,
 });
+
+const userSchema = mongoose.Schema({
+
+})
+
+const questionSchema = mongoose.Schema({
+    email: String,
+    username: String,
+
+
+})
 
 export var testModel = mongoose.model("testObj", testSchema);
