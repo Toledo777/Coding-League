d35649fe8ada7dffdeb948731d3f088d180afe04
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-02-15 10:52:07 -0500

    Remove unused images.jsx, refactor simulated code running, add div containing buttons to skip current problem if code fails or go to next problem if code succeeds.

--- client/routes/Start.jsx
+++ client/routes/Start.jsx
@@ -1,34 +1,55 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import incorrect from '../public/assets/incorrect.png';
 // import correct from '../public/assets/correct.png';
 
 export default function Start() {
 	const [solution, setSolution] = useState('');
-	const [running, setRunning] = useState(false);
+	const [isRunning, setIsRunning] = useState(false);
+	const [isCorrect, setIsCorrect] = useState(false);
+
+	useEffect(() => {
+		loadProblem();
+	});
+
+	function loadProblem() {
+		document.querySelector('.instructions').textContent = 'Proglem: write a method that takes in a String, determines if the string is a palindrome or not, and finally outputs a Boolean result.';
+		document.querySelector('.input').textContent = 'Input: String';
+		document.querySelector('.output').textContent = 'Output: Boolean';
+	}
 
 	function handleSolutionChange(event) {
 		setSolution(event.target.value);
 	}
 
-	function clearSolution(){
+	function clearSolution() {
 		setSolution('');
 	}
 
-	function runSolution(){
-		if(!running){
-			console.log(solution);
+	async function runSolution() {
+		if (!isRunning) {
+			setIsRunning(true);
 			console.log('Running solution...');
-			setRunning(true);
-			setTimeout(() => { 
-				// console.log(incorrect);
-				document.querySelector('.res-img-1').src = 'assets/incorrect.png';
-				document.querySelector('.res-img-2').src = 'assets/incorrect.png';
-				document.querySelector('.res-img-3').src = 'assets/correct.png';
-			}, 5000);
-			setRunning(false);
+			await new Promise(r => setTimeout(r, 500));
+			document.querySelector('.res-img-1').src = 'assets/incorrect.png';
+			await new Promise(r => setTimeout(r, 1500));
+			document.querySelector('.res-img-2').src = 'assets/incorrect.png';
+			await new Promise(r => setTimeout(r, 300));
+			document.querySelector('.res-img-3').src = 'assets/correct.png';
+			await new Promise(r => setTimeout(r, 100));
+			setIsRunning(false);
+			console.log('done!');
+			setIsCorrect(false);
 		} else {
 			console.log('Existing solution already running, wait until it\'s done!!!');
+		}
+		document.querySelector('.after-running').style.visibility = 'visible';
+		if (!isCorrect) {
+			document.querySelector('.completed').textContent = 'Test cases failed!';
+		} else {
+			document.querySelector('.completed').textContent = 'All test cases passed!';
+			document.querySelector('.skip').style.display = 'none';
+			document.querySelector('.next').style.display = 'inline';
 		}
 	}
 
@@ -36,9 +57,9 @@ export default function Start() {
 		<h1>Coding Problems</h1>
 		<div className='problem'>
 			<h2>Problem #1</h2>
-			<div>Proglem: write a method that takes in a String, determines if the string is a palindrome or not, and finally outputs a Boolean result.</div>
-			<div><b>Input:</b> (1) String</div>
-			<div><b>Output:</b> (1) Boolean</div>
+			<div className='instructions'></div>
+			<div className='input'></div>
+			<div className='output'></div>
 		</div>
 		<div><p></p></div>
 		<div className='form-div'>
@@ -57,18 +78,20 @@ export default function Start() {
 		<div className='results'>
 			<div className='test-cases'>
 				<div className='case-1'>
-					<h2 className='expected-1'>racecar (true): </h2>
-					<img className='res-img-1'/>
+					<h2 className='expected-1'>racecar (true): <img className='res-img-1' /></h2>
 				</div>
 				<div className='case-2'>
-					<h2 className='expected-2'>kayak (true): </h2>
-					<img className='res-img-2'/>
+					<h2 className='expected-2'>kayak (true): <img className='res-img-2' /></h2>
 				</div>
 				<div className='case-3'>
-					<h2 className='expected-3'>palindrome (false): </h2>
-					<img className='res-img-3'/>
+					<h2 className='expected-3'>palindrome (false): <img className='res-img-3' /></h2>
 				</div>
 			</div>
+		</div>
+		<div className='after-running' style={{visibility: 'hidden'}}>
+			<p className='completed'></p>
+			<button className='next' style={{display: 'none'}}>Next problem</button>
+			<button className='skip'>Skip problem</button>
 		</div>
 	</div>;
 }
