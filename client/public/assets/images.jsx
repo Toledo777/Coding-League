67b55b948476f7e08eaafcebeb0098ef0e25e847
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-02-15 10:52:07 -0500

    Remove unused images.jsx, refactor simulated code running, add div containing buttons to skip current problem if code fails or go to next problem if code succeeds.

--- client/public/assets/images.jsx
+++
@@ -1,5 +0,0 @@
-const images = [
-	{ id: 1, src: './incorrect.png', title: 'Incorrect', description: 'Red x icon' },
-	{ id: 2, src: './correct.png', title: 'Correct', description: 'Green checkmark icon' }
-];
-export default images;
