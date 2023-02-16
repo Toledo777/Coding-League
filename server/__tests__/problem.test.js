f42367ca1582763edcdaa1c948b8bc567538c371
Matthew Toledo <matthew.toledo20@gmail.com>
2023-02-16 12:46:43 -0500

    edit package json to run unit test

--- server/__tests__/problem.test.js
+++ server/__tests__/problem.test.js
@@ -1,1 +1,6 @@
 import mockingoose from "mockingoose";
+import { problem } from "../models/problem.mjs";
+
+mockingoose(problem).toReturn({}, "findOne");
+
+mockingoose(problem).toReturn(new Error("error"), "save");
