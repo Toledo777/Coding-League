5bc4fc7d2397e1c2ad73a05f8cf3cd15ebb389c8
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-16 11:50:26 -0500

    add ts declaration file for problem

---
+++ web-scraper/types/problem.d.ts
@@ -0,0 +1,20 @@
+type HTML = string;
+
+type TestCase = { input: string, output: string };
+
+export type Problem = {
+    id: string,
+    url: string,
+    title: string,
+    // in seconds
+    time_limit: number,
+    // in megabytes
+    memory_limit: number,
+    description: HTML,
+    input_specification: HTML,
+    output_specification: HTML,
+    note?: HTML,
+    sample_tests: HTML,
+    testcases: TestCase[],
+    tags: string[],
+}
