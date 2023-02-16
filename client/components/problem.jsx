167b75fcac9b7af2e26e1fc8b4c7e8b1e7c48727
alex <alexvendramin01@gmail.com>
2023-02-16 11:57:35 -0500

    using useFetch variables to their fullest extent

--- client/components/problem.jsx
+++ client/components/problem.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import useFetch from '../hooks/useFetch';
 
 export default function Problem(props) {
 
+
+	const [error, loading, data] = useFetch('/api/one-problem?range=' + props.range, 1, []);
+
+	if (error !== undefined) {
+		console.log('there is an error!!!');
+		data.text = error;
+	}
+
+	if (loading == true) {
+		console.log('still loading');
+		data.text = 'loading...';
+	}
+
 	return (
 		<div>
 			<h3>
-				{(useFetch('/api/one-problem?range=' + props.range, 1, [])[2].text)}
+				{data.text}
 			</h3>
 		</div>
 	);
