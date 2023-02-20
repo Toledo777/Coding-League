dee29b047e10823774cf6a7125b34061a4944497
alex <alexvendramin01@gmail.com>
2023-02-20 10:36:27 -0500

    changed url to /problem/random

--- client/components/problem.jsx
+++ client/components/problem.jsx
@@ -5,7 +5,7 @@ import useFetch from '../hooks/useFetch';
 export default function Problem(props) {
 
 
-	const [error, loading, data] = useFetch('/api/problem?range=' + props.range, 1, []);
+	const [error, loading, data] = useFetch('/api/problem/random?range=' + props.range, 1, []);
 
 
 
--- server/api.mjs
+++ server/api.mjs
@@ -3,7 +3,7 @@ const router = express.Router();
 import { problem } from './models/problem.mjs';
 
 
-router.get('/problem', async (req, res) => {
+router.get('/problem/random', async (req, res) => {
 	console.log('problem api');
 	const response = await problem.find();
 	res.json(response[1]);
