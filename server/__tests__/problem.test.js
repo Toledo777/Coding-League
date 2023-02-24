c2c364c1f82086bb4f365a0bde3e1d8a703abe88
Matthew Toledo <matthew.toledo20@gmail.com>
2023-02-24 17:49:22 -0500

    change id to _id

--- server/__tests__/problem.test.js
+++ server/__tests__/problem.test.js
@@ -4,16 +4,15 @@ import { problem } from "../models/problem.mjs";
 describe("test mongoose problem model", () => {
     it("should return the problem with findById", async () => {
         const _prob = {
-            id: "2F18",
+            _id: "2F18",
             title: "What is that noise in the ceiling",
             url: "https://www.dawsoncollege.qc.ca",
             tags: ["School", "Impossible"],
             test_cases: [{input: "hello", output: "world"}]
         } 
 
-
         mockingoose(problem).toReturn(_prob, "findOne");
-        let actualProblem = await problem.findById({id: "2F18"});
+        let actualProblem = await problem.findById({_id: "2F18"});
         return expect(JSON.parse(JSON.stringify(actualProblem))).toMatchObject(_prob);
     });
 });
