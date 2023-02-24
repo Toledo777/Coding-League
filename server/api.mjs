685df54afc38c77e6af2cca70ac1bbdba03676f3
alex <alexvendramin01@gmail.com>
2023-02-24 12:08:51 -0500

    fix no id for sure this time

--- server/api.mjs
+++ server/api.mjs
@@ -2,15 +2,21 @@ import express from 'express';
 const router = express.Router();
 import { problem } from './models/problem.mjs';
 
-
+/**
+ * gets a random problem, works on first 50 problems,
+ *  incomplete until we decide how we want to implement difficulty
+ */
 router.get('/problem/random', async (req, res) => {
 	const response = await problem.find({});
 	let random = Math.floor(Math.random(50));
 	res.json(response[random]);
 });
 
+/**
+ * gets a single problem by its id
+ */
 router.get('/problem/id', async (req, res) => {
-	if (req.query.id !== '' && req.query.id !== null) {
+	if (req.query.id) {
 		const response = await problem.findById({ _id: req.query.id });
 		res.json(response);
 	}
@@ -19,12 +25,22 @@ router.get('/problem/id', async (req, res) => {
 	}
 });
 
+/**
+ * gets a single problem by its title
+ */
 router.get('/problem/title', async (req, res) => {
 	const response = await problem.findOne({ title: req.query.title });
 	res.json(response);
 });
-//preliminary difficulty implementation
+
+
+/**
+ * gets the first problem related to the tag sent by the user,
+ * incomplete until difficulty implementation is complete and I figure out a way to deal with more than one tags with a space in them
+ */
 router.get('/problem/tags', async (req, res) => {
+	//preliminary difficulty implementation
+
 	// if (req.query.difficulty) {
 	// 	console.log('there is difficulty range');
 	// 	const response = await problem.find({});
@@ -32,15 +48,23 @@ router.get('/problem/tags', async (req, res) => {
 	// }
 	// console.log('there is no difficulty');
 
+
+
 	//finding multiple tags possible with $in
 	const response = await problem.find({ tags: { $in: ['\n    ' + req.query.tags + '\n', '\n    ' + '*2300' + '\n'] } });
 	res.json(response[0]);
 });
 
+/**
+ * useless hello world, can be deleted in a later commit
+ */
 router.get('/hello_world', (req, res) => {
 	res.send('Hello World');
 });
 
+/**
+ * post api for client to server posts, going to be used in a different sprint
+ */
 router.post('/answer', (req, res) => {
 	console.log(req.query.answer);
 	res.status(200);
