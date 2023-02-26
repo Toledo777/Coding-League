5a381c40301cda6ace165afc7c517a2879e1a17f
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-26 13:47:00 -0500

    Don't decode more bytes than needed

--- code-runner/src/safe_reader.ts
+++ code-runner/src/safe_reader.ts
@@ -2,7 +2,7 @@ const decoder = new TextDecoder();
 export async function readTextFileWithLimit(path: string, limit: number): Promise<string> {
     const file = await Deno.open(path);
     const buffer = new Uint8Array(limit);
-    await file.read(buffer);
+    const length = await file.read(buffer);
     file.close();
-    return decoder.decode(buffer);
+    return decoder.decode(buffer.subarray(0, length!));
 }
