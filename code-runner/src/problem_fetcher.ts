6978c799af3d3a45d4e9cb30e287586437573eda
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-26 11:06:35 -0500

    Fetch problems from api

--- code-runner/src/problem_fetcher.ts
+++ code-runner/src/problem_fetcher.ts
@@ -1,8 +1,14 @@
 import { Problem } from "./types/problem.d.ts";
-const API_URL = "https://codingleague.azurewebsites.net";
+// const API_URL = "https://codingleague.azurewebsites.net";
+const API_URL = "http://localhost:8080";
 
 export default async function fetchProblemById(id: string): Promise<Problem> {
     const res = await fetch(`${API_URL}/api/problem/id?id=${id}`);
-    const problem = await res.json();
-    return problem;
+    if (res.ok) {
+        console.log("OK!")
+        const problem = await res.json();
+        return problem;
+    } else {
+        throw new Error(res.statusText)
+    }
 }
