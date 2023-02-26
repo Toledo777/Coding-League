ee40c9a025e8c2ea7d3081b180fa0cad49e96e35
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-26 11:21:58 -0500

    remove dummy props

--- client/routes/home.jsx
+++ client/routes/home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Problem from '../components/problem';
 
 export default function Home() {
 	return (
 		<div>
 			<h1>Home!</h1>
-			<Problem id=""></Problem>
 		</div>
 	);
 }
