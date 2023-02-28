709623bf2df3cdfbd315368e38abdb89c97f3460
Noah Labrecque <noah.labrecque@gmail.com>
2023-02-28 08:48:22 -0500

    show active page

--- client/root.jsx
+++ client/root.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export default function Root() {
 	return (
 		<>
 			<nav>
-				<Link to={'/'}>Home</Link>
-				<Link to={'/solve/282A'}>Solve</Link>
+				<NavLink to={'/'}>Home</NavLink>
+				<NavLink to={'/solve/282A'}>Solve</NavLink>
 			</nav>
 			<main className='content'>
 				<Outlet />
