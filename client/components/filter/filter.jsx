b79c42c363b44bcc7f3e9857cfb2140c3aa78459
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-02-28 17:10:22 -0500

    Install 'multi-range-slider-react' package for creating double-ended slider for filtering problem difficulty.

--- client/components/filter/filter.jsx
+++ client/components/filter/filter.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import MultiRangeSlider from 'multi-range-slider-react';
 
 export default function Filter({ setTags, setRange }) {
+	const [minValue, setMinValue] = useState(0);
+	const [maxValue, setMaxValue] = useState(0);
+	const [minValue2, setMinValue2] = useState(0);
+	const [maxValue2, setMaxValue2] = useState(0);
+
+	const handleChangeInput = (e) => {
+		setMinValue(e.minValue);
+		setMaxValue(e.maxValue);
+	};
 
 	function setTagsHelper(event) {
 		let choices = Array.from(event.target.selectedOptions).map(({ value }) => value);
 		setTags(choices);
 	}
 
-	function setRangeHelper(value) {
-		setRange(value);
+	function setRangeHelper(event) {
+		setRange(event);
 		// updateFilter(tags, range);
 	}
 
@@ -16,13 +27,13 @@ export default function Filter({ setTags, setRange }) {
 		<div className='filter-pane'>
 			<h2>Filters</h2>
 			<h3>Tags</h3>
-			<select className="tags" multiple={true} onChange={(event) => setTagsHelper(event)}>
+			<select multiple={true} onChange={(event) => setTagsHelper(event)}>
 				<option value="val1">math</option>
 				<option value="val2">probabilities</option>
 				<option value="val3">data structures</option>
 				<option value="val4">geometry</option>
 			</select>
-			<input className="slider" type="range" min="800" max="2500" value="50"></input>
+			<MultiRangeSlider min={0} max={50} step={2} minValue={minValue} maxValue={maxValue} onInput={(e) => { handleChangeInput(e); }} />
 		</div>
 	);
 }
