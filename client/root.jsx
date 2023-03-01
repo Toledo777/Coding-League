075cf50e977ab91d30c6712f36e8c58b5070cf9c
Matthew Toledo <matthew.toledo20@gmail.com>
2023-03-01 10:26:00 -0500

    add error handling for login id fetch, move login to nav

--- client/root.jsx
+++ client/root.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import useFetch from './hooks/useFetch';
+
 export default function Root() {
+	let [error, loading, data] = useFetch('/api/google-client-id', [])
+
+	// reword error message for user
+	if (error) {
+		error = "Error loading google authentification";
+	}
+
+	
 	return (
-			<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-				<nav>
-					<Link to={'/'}>Home</Link>
-					<Link to={'/solve/282A'}>Solve</Link>
-				</nav>
-				<main className='content'>
-					<Outlet />
-				</main>
-			</GoogleOAuthProvider>
+		<GoogleOAuthProvider clientId={data}>
+			<nav>
+				<Link to={'/'}>Home</Link>
+				<Link to={'/solve/282A'}>Solve</Link>
+				<GoogleLogin/>
+			</nav>
+			<main className='content'>
+				<h3>
+					{error}
+					{loading && 'Loading google authentication'}
+				</h3>
+				<Outlet />
+			</main>
+		</GoogleOAuthProvider>
 	);
 }
--- client/routes/home.jsx
+++ client/routes/home.jsx
@@ -1,10 +1,9 @@
-import { GoogleLogin } from '@react-oauth/google';
+
 import React from 'react';
 
 export default function Home() {
 	return (
 		<div>
-			<GoogleLogin/>
 			<h1>Home!</h1>
 		</div>
 	);
