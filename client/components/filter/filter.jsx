7560c999b77db2197edff1af86765b5677bd4ed7
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-01 11:19:54 -0500

    Create array of tags in filter.jsx and dynamically populate selection field, create dedicated css file with styling for tags realtime list

--- client/components/filter/filter.jsx
+++ client/components/filter/filter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './filter.css';
 import { useState, useEffect } from 'react';
 import MultiRangeSlider from 'multi-range-slider-react';
 // import 'react-range-slider-input/dist/style.css';
@@ -7,6 +8,33 @@ export default function Filter({ tags, setTags, range, setRange }) {
 	// var MultiRangeSlider = require('multi-range-slider-react');yarn add -D @types/react-slider
 	const [minValue, setMinValue] = useState(800);
 	const [maxValue, setMaxValue] = useState(3500);
+
+	const labels = [
+		'math',
+		'probabilities',
+		'data structures',
+		'geometry',
+		'bitmasks',
+		'dfs and similar',
+		'dp',
+		'combinatorics',
+		'binary search',
+		'brute force',
+		'greedy',
+		'sortings',
+		'constructive algorithms',
+		'dsu',
+		'implementation',
+		'strings',
+		'trees',
+		'graphs',
+		'divide and conquer',
+		'two pointers',
+		'fft',
+		'number theory',
+		'hashing',
+		'shortest paths',
+	];
 
 	function setTagsHelper(event) {
 		let choices = Array.from(event.target.selectedOptions).map(({ value }) => value);
@@ -54,12 +82,9 @@ export default function Filter({ tags, setTags, range, setRange }) {
 			<h2>Filters</h2>
 			<h3>Tags</h3>
 			<select multiple={true} onChange={(event) => setTagsHelper(event)}>
-				<option value="math">math</option>
-				<option value="probabilities">probabilities</option>
-				<option value="data structures">data structures</option>
-				<option value="geometry">geometry</option>
+				{labels.length > 0 && labels.map((label, index) => <option key={index} value={label}>{label}</option>)}
 			</select>
-			<ul className='tags' style={{'listStyle': 'none', 'textIndent': '-2em'}}>
+			<ul className='tags'>
 				{tags.length > 0 && tags.map((tag, index) => <li key={index}>{tag} </li>)}
 			</ul>
 			<h3>Difficulty</h3>
