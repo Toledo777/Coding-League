027456b74d1ab0bd751f4676dcc16ca575bae44e
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-01 10:33:44 -0500

    Complete tag filter implementation, implement difficulty range filter (may switch to sliding scale if we can get imports working), move filter realtime results from search component into filter component.

--- client/components/filter/filter.jsx
+++ client/components/filter/filter.jsx
@@ -1,41 +1,75 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-// import { RangeSlider } from 'react-range-slider-input';
+import MultiRangeSlider from 'multi-range-slider-react';
 // import 'react-range-slider-input/dist/style.css';
 
-export default function Filter({ setTags, setRange }) {
-	const [minValue, setMinValue] = useState(0);
-	const [maxValue, setMaxValue] = useState(0);
-	const [minValue2, setMinValue2] = useState(0);
-	const [maxValue2, setMaxValue2] = useState(0);
-
-	const handleChangeInput = (e) => {
-		setMinValue(e.minValue);
-		setMaxValue(e.maxValue);
-	};
+export default function Filter({ tags, setTags, range, setRange }) {
+	// var MultiRangeSlider = require('multi-range-slider-react');yarn add -D @types/react-slider
+	const [minValue, setMinValue] = useState(800);
+	const [maxValue, setMaxValue] = useState(3500);
 
 	function setTagsHelper(event) {
 		let choices = Array.from(event.target.selectedOptions).map(({ value }) => value);
+		if (choices.length === 0){
+			choices = ['all'];
+		}
 		setTags(choices);
 	}
 
-	function setRangeHelper(event) {
-		setRange(event);
-		// updateFilter(tags, range);
+	function handleMinChange(min){
+		if (min < 800){
+			setMinValue(800);
+		}
+		else if (min > maxValue){
+			setMinValue(maxValue);
+		}
+		else {
+			setMinValue(min);
+		}
 	}
+
+	function handleMaxChange(max){
+		if (max > 3500){
+			setMaxValue(3500);
+		}
+		else if (max < minValue){
+			setMaxValue(minValue);
+		}
+		else {
+			setMaxValue(max);
+		}
+	}
+
+	useEffect (() => {
+		setRange([minValue, maxValue]);
+	}, [minValue, maxValue]);
+
+	// function setRangeHelper(event) {
+	// 	setRange(event.minValue, event.maxValue);
+	// 	// updateFilter(tags, range);
+	// }
 
 	return (
 		<div className='filter-pane'>
 			<h2>Filters</h2>
 			<h3>Tags</h3>
 			<select multiple={true} onChange={(event) => setTagsHelper(event)}>
-				<option value="val1">math</option>
-				<option value="val2">probabilities</option>
-				<option value="val3">data structures</option>
-				<option value="val4">geometry</option>
+				<option value="math">math</option>
+				<option value="probabilities">probabilities</option>
+				<option value="data structures">data structures</option>
+				<option value="geometry">geometry</option>
 			</select>
-			
-			{/* <RangeSlider /> */}
+			<ul className='tags' style={{'listStyle': 'none', 'textIndent': '-2em'}}>
+				{tags.length > 0 && tags.map((tag, index) => <li key={index}>{tag} </li>)}
+			</ul>
+			<h3>Difficulty</h3>
+			<div className='range-inputs'>
+				<label htmlFor="min">Min:</label>
+				<input type='number' min='800' max={maxValue} value={minValue} className='min' name="min" onInput={(event) => setMinValue(event.target.value)} onBlur={(event) => handleMinChange(event.target.value)}></input>
+				<label htmlFor="max">Max:</label>
+				<input type='number' min={minValue} max='3500' value={maxValue} className='max' name="max" onInput={(event) => setMaxValue(event.target.value)} onBlur={(event) => handleMaxChange(event.target.value)}></input>
+			</div>
+			<p>Range: {range[0]} - {range[1]}</p>
 		</div>
 	);
 }
--- client/routes/search.jsx
+++ client/routes/search.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react';
 
 export default function Search() {
 	const [search, setSearch] = useState('');
-	const [tags, setTags] = useState(['']);
-	const [range, setRange] = useState([0, 3500]);
+	const [tags, setTags] = useState(['all']);
+	const [range, setRange] = useState([800, 3500]);
 
 	function updateFilter(newTags, newRange){
 		setTags(newTags);
@@ -15,10 +15,8 @@ export default function Search() {
 	return (
 		<div>
 			<h1>Search!</h1>
-			<Filter setTags={setTags} setRange={setRange} />
 			<h2>Search params:</h2>
-			<p>Tags: {tags}</p>
-			<p>Range: {range[0]} - {range[1]}</p>
+			<Filter tags={tags} setTags={setTags} range={range} setRange={setRange} />
 		</div>
 	);
 }
