bfe20ae33edd9932f2377237f8fc23f920dcdf21
Matthew Toledo <matthew.toledo20@gmail.com>
2023-02-28 21:36:31 -0500

    add login component to home

--- client/root.jsx
+++ client/root.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 export default function Root() {
 	return (
-		<>
-			<nav>
-				<Link to={'/'}>Home</Link>
-				<Link to={'/solve/282A'}>Solve</Link>
-			</nav>
-			<main className='content'>
-				<Outlet />
-			</main>
-		</>
+			<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+				<nav>
+					<Link to={'/'}>Home</Link>
+					<Link to={'/solve/282A'}>Solve</Link>
+				</nav>
+				<main className='content'>
+					<Outlet />
+				</main>
+			</GoogleOAuthProvider>
 	);
 }
--- client/routes/home.jsx
+++ client/routes/home.jsx
@@ -1,8 +1,10 @@
+import { GoogleLogin } from '@react-oauth/google';
 import React from 'react';
 
 export default function Home() {
 	return (
 		<div>
+			<GoogleLogin/>
 			<h1>Home!</h1>
 		</div>
 	);
