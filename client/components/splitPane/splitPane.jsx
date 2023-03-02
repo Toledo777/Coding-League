ce44ac35d1f7ebf6820974636fe26564f3ae17fb
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-02 12:10:00 -0500

    split-pane draft

--- client/components/splitPane/splitPane.jsx
+++ client/components/splitPane/splitPane.jsx
@@ -6,17 +6,17 @@ export default function SplitPane({ labels, children }) {
 	const [selected, setSelected] = useState(0);
 
 	return <div className='split-pane'>
-		<div className='pane-selector panel'>{
+		<div className='pane-selector'>{
 			labels.map((label, index) => {
 				return <button
 					key={label}
-					className={(selected == index ? 'selected ' : '') + 'btn pane-button'}
+					className={(selected == index ? 'selected ' : '') + 'pane-button'}
 					onClick={() => setSelected(index)}
 				>{label}</button>;
 			})
 		}
 		</div>
-		<div className='panel pane-content'>
+		<div className='pane-content'>
 			{children[selected]}
 		</div>
 	</div>;
