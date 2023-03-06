01e1393dbacf56a236ea561a28b0df4d18ae57fa
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-06 10:24:43 -0500

    Add status code 200 test case for '/api/allTags/ route.

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -6,4 +6,9 @@ describe('GET /api', () => {
 		const response = await request(app).get('/api/hello_world');
 		expect(response.statusCode).toBe(200);
 	});
+
+	test('All tags route, should return 200', async () => {
+		const response = await request(app).get('/api/allTags');
+		expect(response.statusCode).toBe(200);
+	});
 });
