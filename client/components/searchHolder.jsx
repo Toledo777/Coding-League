be1a9906aa22d7e71408cb454e6158f675393ba8
alex <alexvendramin01@gmail.com>
2023-03-08 09:46:39 -0500

    changing fetch to be in search.jsx

--- client/components/searchHolder.jsx
+++ client/components/searchHolder.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
 import SearchCase from './SearchCase/searchCase';
-import useFetch from '../hooks/useFetch';
 
 
 //count = how many problems to fetch
 //start = page to start on
 //title = search param
 //tags = also search param
-export default function SearchHolder({ count, start, title, tags }) {
-
-	if (title == '') {
-		title = 'A. Two distinct points';
-	}
-	let [error, loading, data] = useFetch('/api/searchProblems?search=' + title, [], [title]);
+export default function SearchHolder({ error, loading, data }) {
 
 
-	console.log(data);
-	if (!Array.isArray(data)) {
-		data = [data];
-	}
 
 	//for the linter
-	console.log(count);
-	console.log(start);
-	console.log(tags);
+	// console.log(count);
+	// console.log(start);
+	// console.log(tags);
 
 	//TODO: implement a list type of "default" problems to show the user on the search page
-	// {
 	// 	[error, loading, data] = useFetch('/api/problem/list?start=' + start + '&count=' + count, []);
-	// 	console.log('inside else');
-	// }
 
 
 	return (
--- client/routes/search.jsx
+++ client/routes/search.jsx
@@ -3,14 +3,21 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import Filter from '../components/filter/filter';
 import { useState } from 'react';
 import SearchHolder from '../components/searchHolder';
+import useFetch from '../hooks/useFetch';
 
 export default function Search() {
 	const [tags, setTags] = useState([]);
 	const [diffRange, setDiffRange] = useState([800, 3500]);
 	const [title, setTitle] = useState('');
 
-	//a log for the linter so diffRance doesn't go unused
+
+	let [error, loading, data] = useFetch('/api/searchProblems?search=' + title, [], [title]);
+
+
+	//a log for the linter so diffRange doesn't go unused
 	console.log(diffRange);
+	console.log(tags);
+
 
 	const filterChange = (newTags, newDiffRange) => {
 		setTags(newTags);
@@ -28,7 +35,7 @@ export default function Search() {
 			<Filter
 				key='filters'
 				filterChange={filterChange} />
-			<SearchHolder count={3} start={0} title={title} tag={tags} ></SearchHolder>
+			<SearchHolder error={error} loading={loading} data={data} ></SearchHolder>
 			<SearchBar titleChange={input => titleChange(input)}> </SearchBar>
 		</div>
 	);
