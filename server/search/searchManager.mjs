f2538f29605a05fa63b6074ae84536a5809d6873
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-08 09:30:06 -0500

    Update Lyra search system to include tags for filtering (difficulty rating is in tags)

--- server/search/searchManager.mjs
+++ server/search/searchManager.mjs
@@ -13,12 +13,13 @@ const db = await create({
 	schema: {
 		_id: 'string',
 		title: 'string',
+		tags: 'string[]',
 	},
 });
 
 export async function searchProblems(term) {
 	const searchResults = await search(db, { term: term, limit: 6 });
-	return searchResults.hits.map((hit) => ({ _id: hit.document._id, title: hit.document.title }));
+	return searchResults.hits.map((hit) => ({ _id: hit.document._id, title: hit.document.title, tags: hit.document.tags }));
 }
 
 export async function insertProblems(items) {
