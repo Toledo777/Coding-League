5bcaca2849e8a6e7e67685db791d12b924102dcf
Matthew Toledo <matthew.toledo20@gmail.com>
2023-03-09 12:49:08 -0500

    create basic user component

--- client/components/user.jsx
+++ client/components/user.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import useFetch from '../hooks/useFetch';
 
 
-// component to display user data, take user as inpt
+// component to display user data, take user as input
 export default function user({user}) {
-    const [userErr, userLoading, user] = useFetch('/api/user/username?=' + username, '');
 
     return(
         <div>
-           
-
+           <h2>Cooluser123 {user.username}</h2>
+           <img src={user.avatar} alt="profile avatar"/>
+           <p>Wins: {user.wins}</p>
+           <p>Losses: {user.losses}</p>
+           <p>Rank {user.rank}</p>
         </div>
     )
 }
