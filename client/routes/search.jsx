05e711b62ee4e7692a31a9f5418cc667809e144d
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-09 12:35:47 -0500

    Simplified filtering of search

--- client/routes/search.jsx
+++ client/routes/search.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar/SearchBar';
 import Filter from '../components/filter/filter';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import SearchHolder from '../components/searchHolder';
 import useFetch from '../hooks/useFetch';
 
@@ -12,53 +12,19 @@ export default function Search() {
 
 	let [error, loading, data] = useFetch('/api/searchProblems?search=' + title, [], [title]);
 
-
-	//a log for the linter so diffRange doesn't go unused
-	console.log(diffRange);
-
-
-	let newData = [];
-	if (data.error === undefined) {
-		newData = data.filter(problem => {
-			let valid = true;
-			tags.forEach(t => {
-				if (!problem.tags.includes(t)) {
-					valid = false;
-				}
-			});
-			if (valid) {
-				return problem;
-			}
-		});
-	}
-
-
-	let newestData = newData;
-	if (data.error === undefined) {
-		newestData = [];
-		newData.map((d) => {
-			let canPush = true;
+	const filteredProblems = useMemo(() => {
+		return data.filter(problem => {
+			return tags.every(t => problem.tags.includes(t));
+		}).filter((d) => {
 			let diff = parseInt(d.tags[d.tags.length - 1].slice(1));
-			if (diff >= diffRange[0] && diff <= diffRange[1]) {
-				console.log('can push');
-			}
-			else {
-				canPush = false;
-			}
-			if (canPush && !newestData.includes(d)) {
-				newestData.push(d);
-			}
+			return diff <= diffRange[1] && diff >= diffRange[0];
 		});
-	}
+	}, [data, tags]);
 
 	const filterChange = (newTags, newDiffRange) => {
 		setTags(newTags);
 		setDiffRange(newDiffRange);
 	};
-
-	function doSearch(input) {
-		setTitle(input);
-	}
 
 	return (
 		<div>
@@ -67,8 +33,8 @@ export default function Search() {
 			<Filter
 				key='filters'
 				filterChange={filterChange} />
-			<SearchBar doSearch={input => doSearch(input)}> </SearchBar>
-			<SearchHolder error={error} loading={loading} data={newestData} ></SearchHolder>
+			<SearchBar doSearch={setTitle}> </SearchBar>
+			<SearchHolder error={error} loading={loading} data={filteredProblems} ></SearchHolder>
 		</div>
 	);
 }
