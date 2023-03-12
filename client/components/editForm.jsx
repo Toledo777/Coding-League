d470f325125d502844a8ea260a09dbee1845216d
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2023-03-12 19:06:23 -0400

    add labels to editForm

--- client/components/editForm.jsx
+++ client/components/editForm.jsx
@@ -5,9 +5,15 @@ export default function EditForm({user}) {
 
     return(
         <form>
+            <label for="username">Username:</label>
             <input type="text" name="username" value={user.username}></input>
+
+            <label for="avatar">Avatar link:</label>
             <input type="text" name="avatar" value={user.avatar}></input>
+
+            <label for="bio">Bio:</label>
             <input type="text" name="bio" value={user.bio}></input>
+            <input type="button" name="submitBtn">Submit</input>
         </form>
     )
 }
