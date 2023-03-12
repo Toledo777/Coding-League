d0d8e49f1bbcc9ec433658503fde896da3c6e0b6
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-12 13:16:21 -0400

    Fix express error routing to only send index.html when client expects HTML.

--- server/app.mjs
+++ server/app.mjs
@@ -12,7 +12,6 @@ app.use(compression());
 app.use(express.static('dist'));
 
 app.use('/api', api);
-
 /**
  * 404 response if sub-query not provided
  */
@@ -23,8 +22,12 @@ app.get('/api', (req, res) => {
 /**
  * React router will deal with the routes client side
  */
-app.get('/*', (_, res) => {
-	res.sendFile('index.html', { root: 'dist' });
+app.get('/*', (req, res) => {
+	if (req.accepts('html')) {
+		res.sendFile('index.html', { root: 'dist' });
+	} else {
+		res.status(404).send('404: Not found');
+	}
 });
 
 export default app;
