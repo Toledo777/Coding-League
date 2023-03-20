a6f8e201b1fce879f6b24a3abf8dfe036e935481
Matthew Toledo <matthew.toledo20@gmail.com>
2023-03-20 12:41:04 -0400

    update user model

--- server/models/user.mjs
+++ server/models/user.mjs
@@ -4,10 +4,7 @@ const userSchema = {
 	email: String,
 	username: String,
 	avatar_uri: String,
-	wins: Number,
-	losses: Number,
-	rank: String,
-	bio: String,
+	exp: Number,
 };
 
 export var user = mongoose.model('user', userSchema);
--- server/models/userAnswer.mjs
+++ server/models/userAnswer.mjs
@@ -3,9 +3,8 @@ import mongoose from 'mongoose';
 const userAnswerSchema = {
 	email: String,
 	problem_id: String,
-	user_answer: String,
+	submission: String,
 	pass_test: Boolean,
-	points: Number,
 };
 
 export var userAnswer = mongoose.model('userAnswer', userAnswerSchema);
