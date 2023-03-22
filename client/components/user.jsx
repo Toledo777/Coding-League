07a5756b20d5a1a14e6b859f7ddf6afe9422a700
Matthew Toledo <matthew.toledo20@gmail.com>
2023-03-22 09:43:25 -0400

    removed unused user fields

--- client/components/user.jsx
+++ client/components/user.jsx
@@ -3,17 +3,11 @@ import React from 'react';
 // component to display user data, take user as input
 export default function User({user}) {
 
-
-    // TODO hide the email field a user is visiting someone elses profile
     return(
         <>
             <div>
                 <h2>{user.username}</h2>
                 <img src={user.avatar} alt="profile avatar"/>
-                <p>Email: {user.email}</p>
-                <p>Bio: {user.bio}</p>
-                <p>Wins: {user.wins}</p>
-                <p>Losses: {user.losses}</p>
                 <p>Rank: {user.rank}</p>
             </div>
             <div id="previous answers">
