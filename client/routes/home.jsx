718733bb22953d3ace7d6e7ca470cf761dbefddb
Robert_G <shadow-of-darknes@live.ca>
2023-03-27 11:11:05 -0400

    handle situation where problem data is not loaded in

--- client/routes/home.jsx
+++ client/routes/home.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router';
 import useFetch from '../hooks/useFetch';
 
@@ -7,11 +7,19 @@ export default function Home() {
 
 
 	let [error, loading, data] = useFetch('/api/problem/random', []);
+	const [msg, setMsg] = useState('');
 	let navigate = useNavigate();
 
 	const routeChange = () => {
-		let path = '/solve/' + data._id;
-		navigate(path);
+		setMsg('');
+		if(data){
+			let path = '/solve/' + data._id;
+			
+			navigate(path);
+		} else {
+			setMsg('Something happend. Please try again');
+		}
+
 	};
 
 
@@ -21,8 +29,9 @@ export default function Home() {
 			<h3>
 				{error && error}
 				{loading && 'loading...'}
+				{msg}
 			</h3>
-			<button onClick={routeChange}>Random Problem :)</button>
+			{!loading && <button onClick={routeChange}>Random Problem :)</button>}
 			<p>Welcome to Coding League. To test your skills navigate to the solve page to try a random coding challenge!</p>
 		</div>
 	);
