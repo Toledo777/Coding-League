ce890e31355a814e61b5cae08440a85225e76142
alex <alexvendramin01@gmail.com>
2023-03-27 11:27:01 -0400

    fix tab implementation on leaderboard

--- client/components/leaderboards/leaderboardGlobal.jsx
+++ client/components/leaderboards/leaderboardGlobal.jsx
@@ -16,18 +16,17 @@ export default function Leaderboard({ global }) {
 
 		//if a user is logged in and they request a local leaderboard, if they arent logged in the api will send them a global one
 		else if (global === 'current') {
-			console.log('setting');
 			setUrl('/api/userNeighbors?count=3');
 		}
 	}, [global]);
 
-
-	[error, loading, users] = useFetch(url, [url]);
+	[error, loading, users] = useFetch(url, [{ id: '', username: 'error', exp: 'no users found' }], [url]);
 
 
 	if (users == undefined || users.count == 0) {
 		users = [{ username: 'error', exp: 'no users found' }];
 	}
+
 
 	return (
 		<div>
