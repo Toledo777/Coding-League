63c3f7594889f4f5f742459daace7a3a2796ed1e
Gabriel Internoscia <gabrielinternoscia@Gabriels-MacBook-Air.local>
2023-03-28 20:20:59 -0400

    Remove unused rate limiting code from dev.mjs

--- server/dev.mjs
+++ server/dev.mjs
@@ -6,19 +6,6 @@ import dbConnect from './lib/database.mjs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import rateLimit from 'express-rate-limit';
-import requestIp from 'request-ip';
-
-app.use(requestIp.mw());
-
-app.use(rateLimit({
-	windowMs: 60 * 1000, // 1 minute
-	max: 30, // limit each IP to 30 requests per windowMs
-	keyGenerator: (req, res) => {
-		return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
-	}
-}));
-
 const PORT = process.env.PORT || 8080;
 
 await dbConnect();
