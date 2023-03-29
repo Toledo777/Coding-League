fdfb2db7969f8ef5285d08bfca568e36f549ba65
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-29 11:33:13 -0400

    Add descriptions to problem view

--- client/components/SearchResult/searchResult.jsx
+++ client/components/SearchResult/searchResult.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 import './searchResult.css';
 export default function SearchResult({ problem }) {
-	const { title, _id } = problem;
+	const { title, _id, description } = problem;
 	const navigate = useNavigate();
 	return (
 		<div className='search_result panel'>
 			<h3>{title}</h3>
+			<p>{description}</p>
 			<button onClick={() => navigate(`/solve/${_id}`)}>
 				Code
 			</button>
