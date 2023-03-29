f372fb8ec16bbeaa80b19eedd81a610a28d2648c
Noah Labrecque <noah.labrecque@gmail.com>
2023-03-29 10:54:28 -0400

    Add sanitized descriptions to lyra-db

--- server/search/searchManager.mjs
+++ server/search/searchManager.mjs
@@ -1,12 +1,18 @@
 import { create, insertBatch, search } from '@lyrasearch/lyra';
-
+import { problem } from '../models/problem.mjs';
 const db = await create({
 	schema: {
 		_id: 'string',
 		title: 'string',
+		description: 'string',
 		tags: 'string[]',
 	},
 });
+
+function sanitizeRawText(text) {
+	return text.replaceAll(/<[^>]*>/g, '') // Remove XML
+		.replaceAll(/\s+/g, ' '); // Remove formatting whitespace
+}
 
 export async function searchProblems(term, limit) {
 	const searchResults = await search(db, { term: term, limit: limit });
@@ -14,5 +20,12 @@ export async function searchProblems(term, limit) {
 }
 
 export async function insertProblems(items) {
+	let perf = performance.now();
+	for (let item of items) {
+		item.description = sanitizeRawText(item.description);
+	}
+
+	console.log(`sanitized in: ${performance.now() - perf}`);
+
 	await insertBatch(db, items, { language: 'english', batchSize: 100 });
 }
