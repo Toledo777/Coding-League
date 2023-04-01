0bbc9923b9a84f2da7a64938746abc2d20005c86
Gabriel Internoscia <gabrielinternoscia@The-Cooler-Gabriels-MacBook-Air-2.local>
2023-04-01 16:36:07 -0400

    Increase line-space for results descriptions text.

--- client/components/SearchResult/searchResult.jsx
+++ client/components/SearchResult/searchResult.jsx
@@ -7,7 +7,9 @@ export default function SearchResult({ problem }) {
 	return (
 		<div className='search_result panel'>
 			<h3>{title}</h3>
-			<p>{description}</p>
+			<div className='description_div'>
+				<p className='description'>{description}</p>
+			</div>
 			<button onClick={() => navigate(`/solve/${_id}`)}>
 				Code
 			</button>
